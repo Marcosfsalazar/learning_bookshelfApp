2405c09b2710962d4bb2d658c68e7f6a4d170894
Marcos Salazar <Marcos1010521@gmail.com>
2020-11-05 10:51:30 -0400

    done-> float button and register activity

--- app/src/main/java/com/example/bookshelf_firebase/MainActivity.java
+++ app/src/main/java/com/example/bookshelf_firebase/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.bookshelf_firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.bookshelf_firebase.view.ActivityCadastroLivro;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.buttonAdd);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ActivityCadastroLivro.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/bookshelf_firebase/view/ActivityCadastroLivro.java
@@ -0,0 +1,16 @@
+package com.example.bookshelf_firebase.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bookshelf_firebase.R;
+
+public class ActivityCadastroLivro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_livro);
+    }
+}
