ca649bbcb7c6e50a3042554dcc7ec1f98ddafdce
Marcos Salazar <Marcos1010521@gmail.com>
2020-11-05 22:05:03 -0400

    ->BuildingAdapter ovodormikk

--- app/src/main/java/com/example/bookshelf_firebase/personalAdapter/BookAdapter.java
+++ app/src/main/java/com/example/bookshelf_firebase/personalAdapter/BookAdapter.java
@@ -1,4 +1,18 @@
 package com.example.bookshelf_firebase.personalAdapter;
 
-public class BookAdapter {
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+
+public class BookAdapter extends FirebaseRecyclerAdapter {
+
+    class BookHolder extends RecyclerView.ViewHolder{
+
+        public BookHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
