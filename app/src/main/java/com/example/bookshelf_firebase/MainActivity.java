d21d4b4534d3165a8483b3ea7f3d8670dafd261a
Marcos Salazar <Marcos1010521@gmail.com>
2020-11-06 09:13:11 -0400

    done-> get firebase data for the recyclerview

--- app/src/main/java/com/example/bookshelf_firebase/MainActivity.java
+++ app/src/main/java/com/example/bookshelf_firebase/MainActivity.java
@@ -1,20 +1,33 @@
 package com.example.bookshelf_firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.bookshelf_firebase.model.Livro;
+import com.example.bookshelf_firebase.personalAdapter.BookAdapter;
 import com.example.bookshelf_firebase.view.ActivityCadastroLivro;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference mDatabase;
+    private BookAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.buttonAdd);
 
@@ -25,5 +38,35 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        setUpRecyclerView();
+
+    }
+
+    private void setUpRecyclerView(){
+        mDatabase = db.getReference().child("pessoa");
+        Query query = mDatabase;
+        FirebaseRecyclerOptions<Livro> options = new FirebaseRecyclerOptions.Builder<Livro>()
+                .setQuery(query,Livro.class)
+                .build();
+
+        adapter = new BookAdapter(options);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
--- app/src/main/java/com/example/bookshelf_firebase/personalAdapter/BookAdapter.java
+++ app/src/main/java/com/example/bookshelf_firebase/personalAdapter/BookAdapter.java
@@ -1,18 +1,50 @@
 package com.example.bookshelf_firebase.personalAdapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.bookshelf_firebase.R;
+import com.example.bookshelf_firebase.model.Livro;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 
-public class BookAdapter extends FirebaseRecyclerAdapter {
+public class BookAdapter extends FirebaseRecyclerAdapter<Livro,BookAdapter.BookHolder> {
+
+    public BookAdapter(@NonNull FirebaseRecyclerOptions<Livro> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull BookHolder bookHolder, int i, @NonNull Livro livro) {
+        bookHolder.txtViewTitle.setText(livro.getNome());
+        bookHolder.txtViewStatus.setText(livro.getStatus());
+        bookHolder.txtViewAutor.setText(livro.getAutor());
+    }
+
+    @NonNull
+    @Override
+    public BookHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_livro,
+                parent,false);
+        return new BookHolder(v);
+    }
 
     class BookHolder extends RecyclerView.ViewHolder{
+        TextView txtViewTitle;
+        TextView txtViewAutor;
+        TextView txtViewStatus;
 
         public BookHolder(@NonNull View itemView) {
+
             super(itemView);
+            txtViewTitle = itemView.findViewById(R.id.txt_view_title);
+            txtViewAutor = itemView.findViewById(R.id.txt_view_autor);
+            txtViewStatus = itemView.findViewById(R.id.txt_view_status);
         }
     }
 }
