c503597a0295576cbb807cb17bd463501ea85118
Marcos Salazar <Marcos1010521@gmail.com>
2020-11-06 16:34:30 -0400

    done -> Update function - Marcos

--- app/src/main/java/com/example/bookshelf_firebase/view/ActivityUpdate.java
+++ app/src/main/java/com/example/bookshelf_firebase/view/ActivityUpdate.java
@@ -4,12 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.example.bookshelf_firebase.R;
 import com.example.bookshelf_firebase.model.Livro;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,6 +26,7 @@ public class ActivityUpdate extends AppCompatActivity {
     private EditText txtQuantidade2;
     private Spinner spnTipo2;
     private Button btnSalvar2;
+    private String saveName;
 
     private Livro livro;
 
@@ -56,6 +59,7 @@ public class ActivityUpdate extends AppCompatActivity {
                     String status = snapshot.child("status").getValue().toString();
                     String parado = "parado";
 
+                    saveName = nome;
                     txtNome2.setText(nome);
                     txtAutor2.setText(autor);
                     txtQuantidade2.setText(quantidade);
@@ -75,6 +79,58 @@ public class ActivityUpdate extends AppCompatActivity {
             }
         });
 
+        initFireBase();
+
+        this.clickButtonListener();
+
+    }
+
+    private void clickButtonListener(){
+        this.btnSalvar2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Livro livro = getLivroForm();
+                ref = FirebaseDatabase.getInstance().getReference("pessoa");
+                ref.child(saveName).child("nome").setValue(livro.getNome());
+                ref.child(saveName).child("autor").setValue(livro.getAutor());
+                ref.child(saveName).child("quantidade").setValue(livro.getQuantidade());
+                ref.child(saveName).child("status").setValue(livro.getStatus());
+
+            }
+        });
+    }
+
+    private void initFireBase(){
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        ref = firebaseDatabase.getReference();
+    }
+
+    private Livro getLivroForm(){
+        this.livro = new Livro();
+
+        if(this.txtNome2.getText().toString().isEmpty() == false){
+            this.livro.setNome(this.txtNome2.getText().toString());
+        }else{
+            return null;
+        }
+
+        if(this.txtAutor2.getText().toString().isEmpty() == false){
+            this.livro.setAutor(this.txtAutor2.getText().toString());
+        }else{
+            return null;
+        }
+
+        if(this.txtQuantidade2.getText().toString().isEmpty() == false){
+            int quantProd = Integer.parseInt(txtQuantidade2.getText().toString());
+            this.livro.setQuantidade(quantProd);
+        }else{
+            return null;
+        }
+
+        this.livro.setStatus(spnTipo2.getSelectedItem().toString());
+
+        return livro;
 
     }
 }
